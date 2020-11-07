c1392a65587e38e2c3c59bae4e7d1381e1035d96
micci_mc <miccicano@gmail.com>
2020-11-06 23:05:53 -0600

    servicio actualizado usuarios

--- MicroservicioUsuarios/src/index.js
+++ MicroservicioUsuarios/src/index.js
@@ -5,9 +5,24 @@ const morgan = require('morgan');
 const myConnection = require('express-myconnection');
 const bodyParser = require('body-parser');
 
+var jwt=require('jsonwebtoken');
+const fs = require('fs');
+const path = require('path');
+
 require('./database');
 
+let respuesta = {
+    error: false,
+    codigo: 200
+   };
 
+
+//TODO ESTO ES PARA DESENCRIPTAR TOKENS QUE SOLICITAN CONSUMIR TUS SERVICIOS DE TU MICROSERVICIO
+var publicKey = fs.readFileSync('./src/public.key','utf8');
+var verifyOptions = {
+    algorithms: ["RS256"],
+    maxAge: "60s"    
+};
 
 
 
@@ -33,71 +48,247 @@ app.listen(3000, ()=>{
     console.log('server on port 3000');
 })
 
-app.get('/users', (req, res) => {
-    req.getConnection((err,conn) => {
-        conn.query('SELECT * from usuario', (err, rows) => {
-            if(err){
-                console.log(err);
-                res.json(err);
-            }
-            res.send(rows);
-        })
-    })
 
+app.post('/prueba', verifytoken, function (req, res) {
+    jwt.verify(req.token,publicKey,verifyOptions,(err)=>{
+        if(err){      
+            respuesta.error=true
+            respuesta.codigo=403
+            res.send(respuesta);
+        }else{
+            var decoded = jwt.decode(req.token, {complete: true});
+
+            req.getConnection((err,conn) => {
+                conn.query('SELECT * FROM usuario', (err, rows) => {
+                    if(err){
+                        console.log(err);
+                        res.json(err);
+                    }
+                    res.json({
+                        header: decoded.header,
+                        payload: decoded.payload,
+                        mensaje: "post fue recibido",
+                        rows,
+                    });
+                });
+            }); 
+            //--
+        }
+    });
+});
+
+//*********************************************************************************************
+//*********************************************************************************************
+//*********************************************************************************************
+//*********************************************************************************************
+//*********************************************************************************************
+
+//devuelve arreglo de jugadores para juegos
+app.post('/listajugadores', verifytoken, function (req, res) {
+    jwt.verify(req.token,publicKey,verifyOptions,(err)=>{
+        if(err){      
+            respuesta.error=true
+            respuesta.codigo=403
+            res.send(respuesta);
+        }else{
+            var decoded = jwt.decode(req.token, {complete: true});
+
+            var cantidadjugadores = 0;
+            var llave = req.body.llave;
+            console.log("llave ", llave);
+            if(llave == 1){
+                cantidadjugadores = 2;
+            }else if(llave == 2){
+                cantidadjugadores = 4;
+            }else if(llave == 3){
+                cantidadjugadores = 8;
+            }else if(llave == 4){
+                cantidadjugadores = 16;
+            }else if(llave == 5){
+                cantidadjugadores = 32;
+            }
+            
+            console.log("cantidad jugadores", cantidadjugadores);
+            req.getConnection((err,conn) => {
+                conn.query('SELECT id FROM usuario ORDER BY RAND() LIMIT ?', cantidadjugadores, (err, rows) => {
+                    if(err){
+                        console.log(err);
+                        res.json(err);
+                    }
+                    res.json({
+                        header: decoded.header,
+                        payload: decoded.payload,
+                        mensaje: "Lista jugadores enviada.",
+                        rows,
+                    });
+                })
+            })
+
+        }
+    });
+});
+
+//agregar un usuario nuevo.
+app.post('/jugadores', verifytoken, function (req, res) {
+    jwt.verify(req.token,publicKey,verifyOptions,(err)=>{
+        if(err){      
+            respuesta.error=true
+            respuesta.codigo=403
+            res.send(respuesta);
+        }else{
+            var decoded = jwt.decode(req.token, {complete: true});
+
+            req.getConnection((err, conn) => {
+                conn.query('INSERT INTO  usuario set ?', req.body, (err, rows) =>{
+                    if(err){
+                        console.log(err);
+                        res.json(err);
+                    }
+                    res.json({
+                        header: decoded.header,
+                        payload: decoded.payload,
+                        mensaje: "Jugador agregado satisfactoriamente",                        
+                    });
+                })
+            });
+            
+            //--
+        }
+    });
+});
+
+//devolver un solo usuario por id
+app.get('/jugadores/:id', verifytoken, function (req, res) {
+    jwt.verify(req.token,publicKey,verifyOptions,(err)=>{
+        if(err){      
+            respuesta.error=true
+            respuesta.codigo=403
+            res.send(respuesta);
+        }else{
+            var decoded = jwt.decode(req.token, {complete: true});
+
+            const  id  = req.params.id;
+            console.log(id);
+            req.getConnection((err, conn) => {
+                conn.query("SELECT * FROM usuario WHERE id = ?", [id], (err, rows) => {
+                    if(err){
+                        console.log(err);
+                        res.json(err);
+                    }
+                    res.json({
+                        header: decoded.header,
+                        payload: decoded.payload,
+                        mensaje: "Jugador devuelto.",
+                        rows: rows[0],
+                    });
+                });
+            });
+            //--
+        }
+    });
+});
+
+//actualizar un usuario
+app.put('/jugadores/:id', verifytoken, function (req, res) {
+    jwt.verify(req.token,publicKey,verifyOptions,(err)=>{
+        if(err){      
+            respuesta.error=true
+            respuesta.codigo=403
+            res.send(respuesta);
+        }else{
+            var decoded = jwt.decode(req.token, {complete: true});
+
+            const id = req.params.id;
+            const user = req.body;
+            req.getConnection((err, conn) => {
+                conn.query('UPDATE usuario set ? where id = ?', [user, id], (err, rows) => {
+                    res.json({
+                        header: decoded.header,
+                        payload: decoded.payload,
+                        mensaje: "Jugador actualizado.",
+                    });
+                });
+            })
+            //--
+        }
+    });
+});
+
+//eliminar un usuario
+app.delete('/jugadores/:id', verifytoken, function (req, res) {
+    jwt.verify(req.token,publicKey,verifyOptions,(err)=>{
+        if(err){      
+            respuesta.error=true
+            respuesta.codigo=403
+            res.send(respuesta);
+        }else{
+            var decoded = jwt.decode(req.token, {complete: true});
+            const id = req.params.id;
+            req.getConnection((err, conn) => {
+                conn.query('DELETE from usuario where id = ?', [id], (err, rows) => {
+                    res.json({
+                        header: decoded.header,
+                        payload: decoded.payload,
+                        mensaje: "Jugador eliminado.",                        
+                    });
+                });
+            });
+            
+            //--
+        }
+    });
+});
+
+//login
+app.get('/login', verifytoken, function (req, res) {
+    jwt.verify(req.token,publicKey,verifyOptions,(err)=>{
+        if(err){      
+            respuesta.error=true
+            respuesta.codigo=403
+            res.send(respuesta);
+        }else{
+            var decoded = jwt.decode(req.token, {complete: true});
+            const user = req.body;
+            req.getConnection((err, conn) => {
+                conn.query("SELECT * FROM usuario WHERE correo = ?", [user.correo], (err, rows) => { 
+                    if(err){
+                        console.log(err);
+                        res.json(err);
+                    }
+                    if(user.password === rows[0].password){
+                        res.json({
+                            header: decoded.header,
+                            payload: decoded.payload,
+                            mensaje: "Login satisfactorio.",
+                            rows,
+                        });
+                    }else{
+                        res.json({
+                            header: decoded.header,
+                            payload: decoded.payload,
+                            mensaje: "Login Error.",
+                            code: 400,
+                        });
+                    }
+                    
+                });
+            });
+            //--
+        }
+    });
 });
 
 
-app.post('/jugadores', (req, res) => {
-    req.getConnection((err, conn) => {
-        conn.query('INSERT INTO  usuario set ?', req.body, (err, rows) =>{
-            if(err){
-                console.log(err);
-                res.json(err);
-            }
-            console.log(req.body.nombres);
-            res.send("exito");
-        })
-    })
-})
 
+// Authorization: Bearer <token>
+function verifytoken(req,res,next){
+    const bearerHeader=req.headers['authorization'];
 
-app.get('/jugadores/:id', (req, res) => {
-    const  id  = req.params.id;
-    console.log(id);
-    req.getConnection((err, conn) => {
-        conn.query("SELECT * FROM usuario WHERE id = ?", [id], (err, rows) => {
-            if(err){
-                console.log(err);
-                res.json(err);
-            }
-            console.log(rows[0].nombres);
-            res.send(rows);
-          });
-    })
-})
-
-app.put('/jugadores/:id', (req, res) => {
-    const id = req.params.id;
-    const user = req.body;
-    req.getConnection((err, conn) => {
-        conn.query('UPDATE usuario set ? where id = ?', [user, id], (err, rows) => {
-            res.send(rows);
-        });
-    })
-})
-
-app.get('/login', (req, res) => {
-    const user = req.body;
-    req.getConnection((err, conn) => {
-        conn.query("SELECT * FROM usuario WHERE correo = ?", [user.correo], (err, rows) => { //modificar para que sea correo y no id
-            if(err){
-                console.log(err);
-                res.json(err);
-            }
-            if(user.password === rows[0].password){
-                res.send(rows[0]);
-            }
-            
-          });
-    })
-})
+    if(typeof bearerHeader !=='undefined'){
+        const bearerToken=bearerHeader.split(" ")[1];
+        req.token=bearerToken;
+        next();
+    }else{
+        res.sendStatus(403) //ruta o acceso prohibido
+    }
+}
